589e984c65412da1e3f196e3d7b1e512597a6307
Sam Wells <sfwells0518@gmail.com>
2023-03-31 15:25:58 -0500

    add delete REST route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get "/cars/:id" => "cars#show"
   post "/cars" => "cars#create"
   patch "/cars/:id" => "cars#update"
-  delete "/"
+  delete "/cars/:id" => "cars#destroy"
 end
