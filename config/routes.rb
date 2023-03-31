bcc399a28bc4cc821f64ebebecc34dcbb34122bd
Sam Wells <sfwells0518@gmail.com>
2023-03-31 15:18:41 -0500

    add update REST route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get "/cars" => "cars#index"
   get "/cars/:id" => "cars#show"
   post "/cars" => "cars#create"
-  # patch
+  patch "/cars/:id" => "cars#update"
   # delete
 end
